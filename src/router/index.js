2e18d81586ee49aba872b0d68f1c5b9bb9a03b7e
谢敏辉 <xmh19940228@163.com>
2018-04-09 17:42:40 +0800

    公用css，router, app.vue
    + app.vue
    + index.js，router使用
    + 公用style, common.scss,mixin.scss,swiper.min.css
    
    Co-Authored-By: Vesper Visonti <xie@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import App from '../App'
 
-import  HelloWorld from '../components/HelloWorld'
-Vue.use(Router)
+const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+export default [{
+    path: '/',
+    component: App, //顶层路由，对应index.html
+    children: [ //二级路由。对应App.vue
+        //地址为空时跳转home页面
+        {
+            path: '',
+            redirect: '/home'
+        },
+        //首页城市列表页
+        {
+            path: '/home',
+            component: home
+        }
+    ]
+}]
