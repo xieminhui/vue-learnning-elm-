9536682da0102ca8cde50b524602a1cd21e72e01
谢敏辉 <xmh19940228@163.com>
2018-04-16 17:59:20 +0800

    增加获取城市名字和搜索地址接口
    m fetchData.js

--- src/service/fetchData.js
+++ src/service/fetchData.js
@@ -15,4 +15,17 @@ export const guessCity = (type) => fetch('/v1/cities', {
 
 export const getUser = () => fetch('/v1/user');
 
+/*
+* 获取所选城市的名字
+* */
+export const getCityInfoByid = (number) =>fetch('/v1/cities/' + number);
+
+/*
+*  搜索地址
+* */
+export const getAddressBySearch = (city_id, value) => fetch('/v1/pois', {
+    type: 'search',
+    city_id: city_id,
+    keyword: value
+})
 
