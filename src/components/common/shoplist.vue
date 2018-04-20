39878f47137ada13dc5793b110ac95eae43dc1c1
谢敏辉 <xmh19940228@163.com>
2018-04-20 17:59:40 +0800

    逐步完善msite页面
    m shoplist.vue
    m env.js
    m mixin.scss

--- src/components/common/shoplist.vue
+++ src/components/common/shoplist.vue
@@ -3,11 +3,11 @@
         <ul v-load-more="loadMore" v-if="shopListArr.length" type="1">
             <router-link :to="{path:'shop', query:{geohash, id:item.id}}" v-for="item in shopListArr" tag='li' :key="item.id" class="shop_li">
                 <section>
-                    <img :src="imgBaseUrl + item.image_path" class="shop_img">
+                    <img :src="shopListImgBaseUrl+ item.image_path" class="shop_img">
                 </section>
                 <section class="shop_right">
                     <header class="shop_detail_header">
-                        <h4 :class="" class="shop_title ellipsis">{{item.name}}</h4>
+                        <h4 :class="item.is_premium? 'premium': '' " class="shop_title ellipsis">{{item.name}}</h4>
                         <!--商家保障-->
                         <ul class="shop_detail_ul">
                             <li v-for="item in item.supports" :key="item.id" class="supports">{{item.icon_name}}</li>
@@ -51,7 +51,7 @@
             </li>
         </ul>
         <p v-if="touchend" class="empty_data">没有更多了</p>
-        <aside class="return_top">
+        <aside class="return_top" @click="" v-if="showBackStatus">
             <svg class="back_top_svg">
                 <use xmlns:xink="http://www.w3.org/1999/xlink" xink:href="#backtop"></use>
             </svg>
@@ -65,27 +65,114 @@
 </template>
 
 <script>
-    import {imgBaseUrl} from "../../config/env"
+    import { mapState } from 'vuex'
+    import {shopListImgBaseUrl} from "../../config/env"
+    import { fetchShopList } from "../../service/fetchData"
+    import ratingStar from './ratingStar'
+    import loading from './loading'
 
     export default {
         data(){
-            shopListArr:[] // 店铺列表数据
+            return{
+                offset: 0, // 批次加载店铺列表，每次加载20个 limit = 20
+                shopListArr:[], // 店铺列表数据
+                shopListImgBaseUrl,
+                preventRepeatReuqest: false,//到达底部加载数据，防止重复加载
+                showBackStatus: false, //显示返回顶部按钮
+                showLoading: true, //显示加载动画
+                touchend: false, //没有更多数据
+
+            }
 
         },
         mounted(){
-
+            this.initData();
         },
-        components(){
-
+        components:{
+            ratingStar,
+            loading
         },
-        props:['geohash'],
+        props:['geohash', 'restaurantCategoryId'],
         mixins:[],
         computed:{
+            //从vuex取经纬度
+            ...mapState([
+                 'latitude','longitude'
+
+             ]),
+        },
+        methods : {
+            async initData(){
+                //获取商铺数据
+                let res = await fetchShopList(this.latitude, this.longitude, this.offset,this.restaurantCategoryId);
+                this.shopListArr = [...res];
+                if(this.shopListArr.length<20){
+                    //每次请求都是20条，小于20说明没数据
+                    this.touchend = true;
+                }
+            },
+            //到达底部加载更多
+            async loaderMore(){
+
+            },
+            zhunshi(){
+
+            }
+
+        },
+        watch : {
 
         }
     }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped type="text/scss">
+    @import "../../style/mixin.scss";
+    .shoplist_container{
+        background: $fc;
+        .shop_li{
+            display: flex;
+            align-items: flex-start;
+            padding: 1.5rem 0.5rem;
+            .shop_img{
+                width: 2.7rem;
+                height: 2.7rem;
+            }
+            .shop_right{
+                flex: auto;
+                margin-left: 0.5rem;
+                .shop_detail_header{
+                    display: flex;
+                    .premium:before{
+                        content: '品牌';
+                        margin-right: 0.2rem;
+                        padding: 0 0.1rem;
+                        border-radius: 0.1rem;
+                        background-color: #ffd930;
+                        font-size: 0.5rem;
+                    }
+                    .shop_title{
+                        @include sc(0.65rem,#333);
+                        align-self: flex-start;
+                        width: 9.5rem;
+                        font-weight: 700;
+                    }
+                    .shop_detail_ul{
+                        display: flex;
+                        align-self: flex-end;
+                        justify-content: flex-end;
+                        .supports{
+                            @include sc(0.4rem, $fzGrey);
+                            padding: 0.01rem;
+                            border: 1px solid $bc;
+                            border-radius: 0.1rem;
+                            margin-right: 0.1rem;
+                        }
+                    }
+                }
 
+            }
+        }
+
+    }
 </style>
--- src/config/env.js
+++ src/config/env.js
@@ -10,6 +10,7 @@
 let baseUrl = '';
 let routerMode = 'history';
 let imgBaseUrl = 'https://fuss10.elemecdn.com';
+let shopListImgBaseUrl = 'http://cangdu.org:8001/img/';
 
 if(process.env.NODE_ENV == 'development'){
 
@@ -20,5 +21,6 @@ if(process.env.NODE_ENV == 'development'){
 export {
     baseUrl,
     routerMode,
-    imgBaseUrl
+    imgBaseUrl,
+    shopListImgBaseUrl
 }
