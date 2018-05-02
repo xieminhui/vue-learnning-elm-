260fc0518237b497444e7bdbc38fb827a376dab4
谢敏辉 <xmh19940228@163.com>
2018-05-02 16:14:04 +0800

    增加图片处理mixin
    m mixin.js

--- src/components/common/mixin.js
+++ src/components/common/mixin.js
@@ -57,3 +57,24 @@ export const loadMore = {
         }
     }
 }
+
+/*
+*  处理图片路径
+* */
+export const getImgPath = {
+    methods: {
+        getImgPath(path){
+            let suffix;
+            if(!path){
+                return 'https://fuss10.elemecdn.com';
+            }
+            if(path.indexOf('jpeg') !== -1){
+                suffix = '.jpeg';
+            }else{
+                suffix = '.png';
+            }
+            let url = '/' + path.substr(0,1) + '/' + path.substr(1, 2) + '/' + path.substr(3) + suffix;
+            return 'https://fuss10.elemecdn.com' + url;
+        }
+    }
+}
