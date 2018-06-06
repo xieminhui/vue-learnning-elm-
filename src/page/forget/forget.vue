871dffeab3ad65592a2d8de863560d4f274b2e39
谢敏辉 <xmh19940228@163.com>
2018-06-06 11:50:14 +0800

    完善forget.vue
    update forget.vue

--- src/page/forget/forget.vue
+++ src/page/forget/forget.vue
@@ -3,15 +3,36 @@
         <head-top head-title="重置密码" goBack="true"></head-top>
         <form class="restForm">
             <section class="input_container phone_number">
-                <input type="text" placeholder="账号" name="phone" maxlength="11" v-model=>
+                <input type="text" placeholder="账号" name="phone" maxlength="11" v-model="phoneNumber" @input="inputPhone">
+            </section>
+            <section class="input_container">
+                <input type="text" placeholder="旧密码" name="oldPassWord" v-model="oldPassWord">
+            </section>
+            <section class="input_container">
+                <input type="text" placeholder="请输入新密码" name="newPassWord" v-model="newPassWord">
+            </section>
+            <section class="input_container">
+                <input text="text" placeholder="请确认密码" name="confirmPassWord" v-model="confirmPassWord">
+            </section>
+            <section class="input_container captcha_code_container">
+                <input type="text" placeholder="验证码" maxlength="6" v-model="mobileCode">
+                <div class="img_change_img">
+                    <img v-show="captchaCodeImg" :src="captchaCodeImg">
+                    <div class="change_img" @click="getCaptchaCode">
+                        <p>看不清</p>
+                        <p>换一张</p>
+                    </div>
+                </div>
             </section>
         </form>
+        <div class="login_container" @click="resetButton">确认修改</div>
+        <alert-tip v-if="showAlert" :showHide="showAlert" @closeTip="showAlert = false" :alertText="alertText"></alert-tip>
     </div>
 </template>
 
 <script>
     import headTop from '../../components/header/header.vue'
-
+    import alertTip from '../../components/common/alertTip.vue'
     export default {
         data(){
             return{
@@ -36,7 +57,8 @@
 
         },
         components:{
-            headTop
+            headTop,
+            alertTip
         },
         computed:{
 
